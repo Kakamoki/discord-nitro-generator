b3c77a4699be4c9c3c7a064b7610deeba11f3c6f
nxyako <78211895+Kakamoki@users.noreply.github.com>
2023-03-28 18:35:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     //Token de votre bot
-    token: "ODUzMjE5MzgxNTMzODY4MDc0.G9EtE6.bppqETD4Qrv2jJzP_gqHbIPBK7lGPv0Wk-7rAs",
+    token: "TOKEN DU BOT",
     //Prefix de votre bot
     prefix: "+",
     //Status de votre bot
     status: [
-"Discord Nitro Boost 3 mois",
+"Discord Nitro Générateur",
 "Nxyako",
 "Gen Bot"
 ]
